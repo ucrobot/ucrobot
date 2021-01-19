5a64df2c6ead6707201a99b49e9f016c7eeb8112
ucrobot <ucrobot@ucrobot.com>
2021-01-19 11:01:59 +0800

    2nd update the robotics' link path

--- head.js
+++ head.js
@@ -1,5 +1,4 @@
 document.write(`
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>RobotStudy</title>
-  <link rel="stylesheet" href="style.css" />
+  <link rel="stylesheet" href="https://ucrobot.github.io/style.css" />
 `);
