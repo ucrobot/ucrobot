cd66c130c86edbfb285f4e6b912e242bda543c16
ucrobot <ucrobot@ucrobot.com>
2021-01-19 10:39:31 +0800

    test link at git page online

--- head.js
+++ head.js
@@ -1,5 +1,5 @@
 document.write(`
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>RobotStudy</title>
-  <link rel="stylesheet" href="/Users/lhh/Documents/Github/ucrobot.github.io/style.css" />
+  <link rel="stylesheet" href="style.css" />
 `);
--- menu.js
+++ menu.js
@@ -1,136 +1,184 @@
 class linkConnect {
-  index() {
-    var varHome = new String("file:///Users/lhh/Documents/Github/ucrobot.github.io/");
-    var varIndex = varHome + "index.html";
-    // location.href = "file:///Users/lhh/Documents/Github/ucrobot.github.io/index.html";
-    location.href = varIndex;
-  };
-  indexRobotics() {
-    var varHome = new String("file:///Users/lhh/Documents/Github/ucrobot.github.io/");
-    var varIndex = varHome + "robotics/robotics.html";
-    location.href = varIndex;
-  };
-  indexRoboticsP() {
-    var varHome = new String("file:///Users/lhh/Documents/Github/ucrobot.github.io/");
-    var varIndex = varHome + "robotics/prek-k/robotics-p.html";
-    location.href = varIndex;
-  };
-  indexRoboticsE() {
-    var varHome = new String("file:///Users/lhh/Documents/Github/ucrobot.github.io/");
-    var varIndex = varHome + "robotics/elementary/robotics-e.html";
-    location.href = varIndex;
-  };
-  indexRoboticsM() {
-    var varHome = new String("file:///Users/lhh/Documents/Github/ucrobot.github.io/");
-    var varIndex = varHome + "robotics/middle/robotics-m.html";
-    location.href = varIndex;
-  };
-  indexRoboticsH() {
-    var varHome = new String("file:///Users/lhh/Documents/Github/ucrobot.github.io/");
-    var varIndex = varHome + "robotics/high/robotics-h.html";
-    location.href = varIndex;
-  };
+	index() {
+		var varHome = new String('https://ucrobot.github.io/');
+		var varIndex = varHome + 'index.html';
+		// location.href = "file:///Users/lhh/Documents/Github/ucrobot.github.io/index.html";
+		location.href = varIndex;
+	}
+	indexRobotics() {
+		var varHome = new String('https://ucrobot.github.io/');
+		var varIndex = varHome + 'robotics/robotics.html';
+		location.href = varIndex;
+	}
+	indexRoboticsP() {
+		var varHome = new String(
+			'file:///Users/lhh/Documents/Github/ucrobot.github.io/'
+		);
+		var varIndex = varHome + 'robotics/prek-k/robotics-p.html';
+		location.href = varIndex;
+	}
+	indexRoboticsE() {
+		var varHome = new String(
+			'file:///Users/lhh/Documents/Github/ucrobot.github.io/'
+		);
+		var varIndex = varHome + 'robotics/elementary/robotics-e.html';
+		location.href = varIndex;
+	}
+	indexRoboticsM() {
+		var varHome = new String(
+			'file:///Users/lhh/Documents/Github/ucrobot.github.io/'
+		);
+		var varIndex = varHome + 'robotics/middle/robotics-m.html';
+		location.href = varIndex;
+	}
+	indexRoboticsH() {
+		var varHome = new String(
+			'file:///Users/lhh/Documents/Github/ucrobot.github.io/'
+		);
+		var varIndex = varHome + 'robotics/high/robotics-h.html';
+		location.href = varIndex;
+	}
 
-  indexMechanics() {
-    var varHome = new String("file:///Users/lhh/Documents/Github/ucrobot.github.io/");
-    var varIndex = varHome + "mechanics/mechanics.html";
-    location.href = varIndex;
-  };
-  indexMechanicsP() {
-    var varHome = new String("file:///Users/lhh/Documents/Github/ucrobot.github.io/");
-    var varIndex = varHome + "mechanics/prek-k/mechanics-p.html";
-    location.href = varIndex;
-  };
-  indexMechanicsE() {
-    var varHome = new String("file:///Users/lhh/Documents/Github/ucrobot.github.io/");
-    var varIndex = varHome + "mechanics/elementary/mechanics-e.html";
-    location.href = varIndex;
-  };
-  indexMechanicsM() {
-    var varHome = new String("file:///Users/lhh/Documents/Github/ucrobot.github.io/");
-    var varIndex = varHome + "mechanics/middle/mechanics-m.html";
-    location.href = varIndex;
-  };
-  indexMechanicsH() {
-    var varHome = new String("file:///Users/lhh/Documents/Github/ucrobot.github.io/");
-    var varIndex = varHome + "mechanics/high/mechanics-h.html";
-    location.href = varIndex;
-  };
-  indexElectronics() {
-    var varHome = new String("file:///Users/lhh/Documents/Github/ucrobot.github.io/");
-    var varIndex = varHome + "electronics/electronics.html";
-    location.href = varIndex;
-  };
-  indexElectronicsP() {
-    var varHome = new String("file:///Users/lhh/Documents/Github/ucrobot.github.io/");
-    var varIndex = varHome + "electronics/prek-k/electronics-p.html";
-    location.href = varIndex;
-  };
-  indexElectronicsE() {
-    var varHome = new String("file:///Users/lhh/Documents/Github/ucrobot.github.io/");
-    var varIndex = varHome + "electronics/elementary/electronics-e.html";
-    location.href = varIndex;
-  };
-  indexElectronicsM() {
-    var varHome = new String("file:///Users/lhh/Documents/Github/ucrobot.github.io/");
-    var varIndex = varHome + "electronics/middle/electronics-m.html";
-    location.href = varIndex;
-  };
-  indexElectronicsH() {
-    var varHome = new String("file:///Users/lhh/Documents/Github/ucrobot.github.io/");
-    var varIndex = varHome + "electronics/high/electronics-h.html";
-    location.href = varIndex;
-  };
-  indexCoding() {
-    var varHome = new String("file:///Users/lhh/Documents/Github/ucrobot.github.io/");
-    var varIndex = varHome + "coding/coding.html";
-    location.href = varIndex;
-  };
-  indexCodingP() {
-    var varHome = new String("file:///Users/lhh/Documents/Github/ucrobot.github.io/");
-    var varIndex = varHome + "coding/prek-k/coding-p.html";
-    location.href = varIndex;
-  };
-  indexCodingE() {
-    var varHome = new String("file:///Users/lhh/Documents/Github/ucrobot.github.io/");
-    var varIndex = varHome + "coding/elementary/coding-e.html";
-    location.href = varIndex;
-  };
-  indexCodingM() {
-    var varHome = new String("file:///Users/lhh/Documents/Github/ucrobot.github.io/");
-    var varIndex = varHome + "coding/middle/coding-m.html";
-    location.href = varIndex;
-  };
-  indexCodingH() {
-    var varHome = new String("file:///Users/lhh/Documents/Github/ucrobot.github.io/");
-    var varIndex = varHome + "coding/high/coding-h.html";
-    location.href = varIndex;
-  };
-  indexRobotkit() {
-    var varHome = new String("file:///Users/lhh/Documents/Github/ucrobot.github.io/");
-    var varIndex = varHome + "robotkit/robotkit.html";
-    location.href = varIndex;
-  };
-  indexRobotkitP() {
-    var varHome = new String("file:///Users/lhh/Documents/Github/ucrobot.github.io/");
-    var varIndex = varHome + "robotkit/prek-k/robotkit-p.html";
-    location.href = varIndex;
-  };
-  indexRobotkitE() {
-    var varHome = new String("file:///Users/lhh/Documents/Github/ucrobot.github.io/");
-    var varIndex = varHome + "robotkit/elementary/robotkit-e.html";
-    location.href = varIndex;
-  };
-  indexRobotkitM() {
-    var varHome = new String("file:///Users/lhh/Documents/Github/ucrobot.github.io/");
-    var varIndex = varHome + "robotkit/middle/robotkit-m.html";
-    location.href = varIndex;
-  };
-  indexRobotkitH() {
-    var varHome = new String("file:///Users/lhh/Documents/Github/ucrobot.github.io/");
-    var varIndex = varHome + "robotkit/high/robotkit-h.html";
-    location.href = varIndex;
-  };
+	indexMechanics() {
+		var varHome = new String(
+			'file:///Users/lhh/Documents/Github/ucrobot.github.io/'
+		);
+		var varIndex = varHome + 'mechanics/mechanics.html';
+		location.href = varIndex;
+	}
+	indexMechanicsP() {
+		var varHome = new String(
+			'file:///Users/lhh/Documents/Github/ucrobot.github.io/'
+		);
+		var varIndex = varHome + 'mechanics/prek-k/mechanics-p.html';
+		location.href = varIndex;
+	}
+	indexMechanicsE() {
+		var varHome = new String(
+			'file:///Users/lhh/Documents/Github/ucrobot.github.io/'
+		);
+		var varIndex = varHome + 'mechanics/elementary/mechanics-e.html';
+		location.href = varIndex;
+	}
+	indexMechanicsM() {
+		var varHome = new String(
+			'file:///Users/lhh/Documents/Github/ucrobot.github.io/'
+		);
+		var varIndex = varHome + 'mechanics/middle/mechanics-m.html';
+		location.href = varIndex;
+	}
+	indexMechanicsH() {
+		var varHome = new String(
+			'file:///Users/lhh/Documents/Github/ucrobot.github.io/'
+		);
+		var varIndex = varHome + 'mechanics/high/mechanics-h.html';
+		location.href = varIndex;
+	}
+	indexElectronics() {
+		var varHome = new String(
+			'file:///Users/lhh/Documents/Github/ucrobot.github.io/'
+		);
+		var varIndex = varHome + 'electronics/electronics.html';
+		location.href = varIndex;
+	}
+	indexElectronicsP() {
+		var varHome = new String(
+			'file:///Users/lhh/Documents/Github/ucrobot.github.io/'
+		);
+		var varIndex = varHome + 'electronics/prek-k/electronics-p.html';
+		location.href = varIndex;
+	}
+	indexElectronicsE() {
+		var varHome = new String(
+			'file:///Users/lhh/Documents/Github/ucrobot.github.io/'
+		);
+		var varIndex = varHome + 'electronics/elementary/electronics-e.html';
+		location.href = varIndex;
+	}
+	indexElectronicsM() {
+		var varHome = new String(
+			'file:///Users/lhh/Documents/Github/ucrobot.github.io/'
+		);
+		var varIndex = varHome + 'electronics/middle/electronics-m.html';
+		location.href = varIndex;
+	}
+	indexElectronicsH() {
+		var varHome = new String(
+			'file:///Users/lhh/Documents/Github/ucrobot.github.io/'
+		);
+		var varIndex = varHome + 'electronics/high/electronics-h.html';
+		location.href = varIndex;
+	}
+	indexCoding() {
+		var varHome = new String(
+			'file:///Users/lhh/Documents/Github/ucrobot.github.io/'
+		);
+		var varIndex = varHome + 'coding/coding.html';
+		location.href = varIndex;
+	}
+	indexCodingP() {
+		var varHome = new String(
+			'file:///Users/lhh/Documents/Github/ucrobot.github.io/'
+		);
+		var varIndex = varHome + 'coding/prek-k/coding-p.html';
+		location.href = varIndex;
+	}
+	indexCodingE() {
+		var varHome = new String(
+			'file:///Users/lhh/Documents/Github/ucrobot.github.io/'
+		);
+		var varIndex = varHome + 'coding/elementary/coding-e.html';
+		location.href = varIndex;
+	}
+	indexCodingM() {
+		var varHome = new String(
+			'file:///Users/lhh/Documents/Github/ucrobot.github.io/'
+		);
+		var varIndex = varHome + 'coding/middle/coding-m.html';
+		location.href = varIndex;
+	}
+	indexCodingH() {
+		var varHome = new String(
+			'file:///Users/lhh/Documents/Github/ucrobot.github.io/'
+		);
+		var varIndex = varHome + 'coding/high/coding-h.html';
+		location.href = varIndex;
+	}
+	indexRobotkit() {
+		var varHome = new String(
+			'file:///Users/lhh/Documents/Github/ucrobot.github.io/'
+		);
+		var varIndex = varHome + 'robotkit/robotkit.html';
+		location.href = varIndex;
+	}
+	indexRobotkitP() {
+		var varHome = new String(
+			'file:///Users/lhh/Documents/Github/ucrobot.github.io/'
+		);
+		var varIndex = varHome + 'robotkit/prek-k/robotkit-p.html';
+		location.href = varIndex;
+	}
+	indexRobotkitE() {
+		var varHome = new String(
+			'file:///Users/lhh/Documents/Github/ucrobot.github.io/'
+		);
+		var varIndex = varHome + 'robotkit/elementary/robotkit-e.html';
+		location.href = varIndex;
+	}
+	indexRobotkitM() {
+		var varHome = new String(
+			'file:///Users/lhh/Documents/Github/ucrobot.github.io/'
+		);
+		var varIndex = varHome + 'robotkit/middle/robotkit-m.html';
+		location.href = varIndex;
+	}
+	indexRobotkitH() {
+		var varHome = new String(
+			'file:///Users/lhh/Documents/Github/ucrobot.github.io/'
+		);
+		var varIndex = varHome + 'robotkit/high/robotkit-h.html';
+		location.href = varIndex;
+	}
 }
 let link = new linkConnect();
 
