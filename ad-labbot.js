66208322e2c6ea78e0df883ba8d29d01cbb23e48
ucrobot <ucrobot@ucrobot.com>
2021-01-27 12:11:48 +0800

    add eq-robo, modify ad.css

---
+++ ad-labbot.js
@@ -0,0 +1,10 @@
+document.write(`
+<img src="https://ucrobot.github.io/img/1-9.jpg" width=150px" />
+<p style="padding-top:0.5rem; padding-bottom:0.5rem; background-color:#808b4d;">
+  &nbsp;&nbsp;Order to:
+</p>
+<a href="mailto:ucrobot@ucrobot.com?cc=gon0071@naver.com&cc=153058927@qq.com">
+  ucrobot@ucrobot.com</a>
+<p style="padding-bottom: 0.5rem;">
+</p>
+`);
